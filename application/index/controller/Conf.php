23b6cf510117c4706db32ee5c005779342b9a065
zhuzr <zhuzr@yilan.tv>
2018-11-07 19:50:10 +0800

    single

---
+++ application/index/controller/Conf.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\index\controller;
+
+class Conf {
+    public static $bannerMsg=array(
+        'BANNER'=>'Electronic Store, <span>Special Offers</span>',
+        'BANNER1'=>'Great Offers on <span>Mobiles</span> Flat <i>35% Discount</i>',      
+    );
+    
+    public static $breadCrumb=array(
+        "SINGLE" => "Single",
+        "PRODUCTS" => "Products",
+        "ABOUTUS" => "About us",
+    );
+}
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,15 +5,26 @@ class Index extends \think\Controller
 {
     public function index()
     {
+        $this->view->assign("bannerMsg", Conf::$bannerMsg['BANNER']);
         return $this->fetch();
     }
 
     public function about()
     {
+        $this->view->assign("bannerMsg", Conf::$bannerMsg['BANNER1']);
+        $this->view->assign("breadMsg", Conf::$breadCrumb['ABOUTUS']);
         return $this->fetch();
     }
     
     public function products(){
+        $this->view->assign("bannerMsg", Conf::$bannerMsg['BANNER1']);
+        $this->view->assign("breadMsg", Conf::$breadCrumb['PRODUCTS']);
+        return $this->fetch();
+    }
+    
+    public function single(){
+        $this->view->assign("bannerMsg", Conf::$bannerMsg['BANNER1']);
+        $this->view->assign("breadMsg", Conf::$breadCrumb['SINGLE']);
         return $this->fetch();
     }
 }
